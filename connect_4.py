76027b552963062128d48c54cbb7c8d0767ecb55
umang <umang.rastogi96@gmail.com>
2020-04-22 19:02:37 -0400

    [connect_4]Add comments for better documentation

--- connect_4.py
+++ connect_4.py
@@ -2,9 +2,14 @@ from utils.game_gui import *
 
 
 if __name__ == '__main__':
+    # Initialize GUI screen and draw the game board on it
     gui = Connect4GUI()
     gui.draw_board()
+    # Initialize game-over parameter with False to start game
     game_over = False
+    # Start with the first player
     player = 0
+    # Play until somebody wins
     while not game_over:
-        player, game_over = gui.check_event(player)
+        # Keep checking for events inside the game
+        game_over, player = gui.check_event(game_over, player)
