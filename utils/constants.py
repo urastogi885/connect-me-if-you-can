bd93a3f87c67e0cd9777f0526b0184f165acbec1
umang <umang.rastogi96@gmail.com>
2020-04-22 17:13:44 -0400

    Define various project constants

---
+++ utils/constants.py
@@ -0,0 +1,14 @@
+# Row-by-column representation of the board
+BOARD_SIZE = 6, 7
+# Define size of each cell in the GUI of the game
+CELL_SIZE = 100
+# Define radius of dot
+RADIUS = (CELL_SIZE // 2) - 5
+# Define size of GUI screen
+GUI_SIZE = (BOARD_SIZE[0] + 1) * CELL_SIZE, (BOARD_SIZE[1]) * CELL_SIZE
+# Define various colors used on the GUI of the game
+RED = 255, 0, 0
+BLUE = 0, 0, 255
+BLACK = 0, 0, 0
+WHITE = 255, 255, 255
+YELLOW = 255, 255, 0
