d85ca1239e8539e116c590d63685c27f710427b8
umang <umang.rastogi96@gmail.com>
2020-04-23 18:11:52 -0400

    [gampe.py] Create method to check for wins along the major diagonal
    Split diagonal-win-checking into 2 methods

--- utils/game.py
+++ utils/game.py
@@ -43,7 +43,7 @@ class Game:
         :return: boolean value whether the last move was a winning move
         """
         return (self.check_horizontal_win(row, col, player) or self.check_vertical_win(row, col, player) or
-                self.check_diagonal_win(row, col, player))
+                self.check_diagonal_win(row, col, player) or self.check_major_diagonal(row, col, player))
 
     def check_horizontal_win(self, row, col, player):
         """
@@ -56,6 +56,7 @@ class Game:
         # Define count to keep track of adjacent tokens of the player
         count = 1
         # Define variable to store the column index of the next adjacent token
+        # Instantiate it with the column index of the last-placed token
         next_col = col
         # Define variable to check for tokens ahead of the last placed token
         is_forward = True
@@ -116,33 +117,33 @@ class Game:
         """
         # Checks for the positively-sloped diagonal wins
         # If all pieces lie ahead of the last dropped piece
-        try:
-            if (self.board[row + 1][col + 1] == player + 1 and self.board[row + 2][col + 2] == player + 1 and
-                    self.board[row + 3][col + 3] == player + 1):
-                return True
-        except IndexError:
-            pass
-        # If 2 pieces lie ahead and 1 piece lies behind the last dropped piece
-        try:
-            if (self.board[row + 1][col + 1] == player + 1 and self.board[row + 2][col + 2] == player + 1 and
-                    self.board[row - 1][col - 1] == player + 1):
-                return True
-        except IndexError:
-            pass
-        # If 1 piece lies ahead and 2 pieces lie behind the last dropped piece
-        try:
-            if (self.board[row - 1][col - 1] == player + 1 and self.board[row - 2][col - 2] == player + 1 and
-                    self.board[row + 1][col + 1] == player + 1):
-                return True
-        except IndexError:
-            pass
-        # If all pieces lie behind the last dropped piece
-        try:
-            if (self.board[row - 1][col - 1] == player + 1 and self.board[row - 2][col - 2] == player + 1 and
-                    self.board[row - 3][col - 3] == player + 1):
-                return True
-        except IndexError:
-            pass
+        # try:
+        #     if (self.board[row + 1][col + 1] == player + 1 and self.board[row + 2][col + 2] == player + 1 and
+        #             self.board[row + 3][col + 3] == player + 1):
+        #         return True
+        # except IndexError:
+        #     pass
+        # # If 2 pieces lie ahead and 1 piece lies behind the last dropped piece
+        # try:
+        #     if (self.board[row + 1][col + 1] == player + 1 and self.board[row + 2][col + 2] == player + 1 and
+        #             self.board[row - 1][col - 1] == player + 1):
+        #         return True
+        # except IndexError:
+        #     pass
+        # # If 1 piece lies ahead and 2 pieces lie behind the last dropped piece
+        # try:
+        #     if (self.board[row - 1][col - 1] == player + 1 and self.board[row - 2][col - 2] == player + 1 and
+        #             self.board[row + 1][col + 1] == player + 1):
+        #         return True
+        # except IndexError:
+        #     pass
+        # # If all pieces lie behind the last dropped piece
+        # try:
+        #     if (self.board[row - 1][col - 1] == player + 1 and self.board[row - 2][col - 2] == player + 1 and
+        #             self.board[row - 3][col - 3] == player + 1):
+        #         return True
+        # except IndexError:
+        #     pass
         # Checks for the negatively-sloped diagonal wins
         # If all pieces lie behind the last dropped piece
         try:
@@ -172,4 +173,53 @@ class Game:
                 return True
         except IndexError:
             pass
+        return False
+
+    def check_major_diagonal(self, row, col, player):
+        """
+        Method to check for winning move along the principal major diagonal
+        :param row: index of the row where the last token of the player was placed
+        :param col: index of the column where the last token of the player was placed
+        :param player: player whose token was placed last
+        :return: boolean value whether the last move was a winning move
+        """
+        # Define count to keep track of adjacent tokens of the player
+        count = 1
+        # Define variables to store the location of the next adjacent token
+        # Instantiate them with location of the last-placed token
+        next_row, next_col = row, col
+        # Define variable to check for tokens ahead of the last placed token
+        is_forward = True
+        # Loop to iterate for tokens ahead of the last placed token
+        # Do not run loop more than 3 times
+        while is_forward or next_row != row + 3 or next_col != col + 3:
+            # Update the location of the next token
+            next_row += 1
+            next_col += 1
+            # Next location should be within in the board and
+            # Verify that next token is the player's token
+            if next_row < BOARD_SIZE[0] and next_col < BOARD_SIZE[1] and self.board[next_row][next_col] == player + 1:
+                count += 1
+            # Break out of loop and check in opposite direction
+            else:
+                is_forward = False
+                next_row = row
+                next_col = col
+                break
+        # Loop to iterate for tokens behind the last placed token
+        # Do not run loop more than 3 times
+        while not is_forward or next_row != row - 3 or next_col != col - 3:
+            # Update the location of the next token
+            next_row -= 1
+            next_col -= 1
+            # Next location should be within in the board and
+            # Verify that next token is the player's token
+            if next_row >= 0 and next_col >= 0 and self.board[next_row][next_col] == player + 1:
+                count += 1
+            # Break out of loop
+            else:
+                break
+        # Player wins if there are 4 adjacent tokens
+        if count == 4:
+            return True
         return False
