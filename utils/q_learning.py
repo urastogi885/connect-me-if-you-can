4734f0654af0a2a1227abf429219e312bb3eb482
umang <umang.rastogi96@gmail.com>
2020-04-25 18:08:08 -0400

    [q_learning.py] Create method stubs for agent class

---
+++ utils/q_learning.py
@@ -0,0 +1,16 @@
+from utils.game import Game
+from utils.constants import BOARD_SIZE
+
+
+class QAgent:
+    def __init__(self, ):
+        pass
+
+    def load_memory(self):
+        pass
+
+    def get_optimal_move(self):
+        pass
+
+    def train(self):
+        pass
