a0acd0e4b92c4b6f3eab87d7d2f9b449562ed84a
umang <umang.rastogi96@gmail.com>
2020-04-26 17:41:47 -0400

    [connect_4.py] Call methods to display menu when game launches

--- connect_4.py
+++ connect_4.py
@@ -4,6 +4,7 @@ from utils.game_gui import GameGUI
 if __name__ == '__main__':
     # Initialize GUI screen and draw the game board on it
     gui = GameGUI()
+    game_mode = gui.main_menu()
     gui.draw_board()
     # Initialize game-over parameter with False to start game
     game_over = False
@@ -12,4 +13,4 @@ if __name__ == '__main__':
     # Play until somebody wins
     while not game_over:
         # Keep checking for events inside the game
-        game_over, player = gui.check_event(game_over, player)
+        game_over, player = gui.run_game(game_mode, player, game_over)
