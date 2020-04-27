c1ae626392ca35781ee18448922d5a86e14db39d
umang <umang.rastogi96@gmail.com>
2020-04-27 11:37:00 -0400

    [connect_4.py] Set training flag to true

--- connect_4.py
+++ connect_4.py
@@ -6,11 +6,4 @@ if __name__ == '__main__':
     gui = GameGUI()
     game_mode = gui.main_menu()
     gui.draw_board()
-    # Initialize game-over parameter with False to start game
-    game_over = False
-    # Start with the first player
-    player = 0
-    # Play until somebody wins
-    while not game_over:
-        # Keep checking for events inside the game
-        game_over, player = gui.run_game(game_mode, player, game_over, True)
+    gui.run_game(game_mode, train=True)
